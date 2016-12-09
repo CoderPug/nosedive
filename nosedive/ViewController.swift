791c918bcbdf55b0675e76d4dc7746cd9e074f78
Jose Torres <jose.torres@santexgroup.com>
2016-12-09 10:45:50 -0500

    Added base reproduction implementation.

--- nosedive/ViewController.swift
+++ nosedive/ViewController.swift
@@ -7,11 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
+
+enum Sound: String {
+    case one = "nosedive_1_star"
+    case two = "nosedive_2_stars"
+    case three = "nosedive_3_stars"
+    case four = "nosedive_4_stars"
+    case five = "nosedive_5_stars"
+}
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    func play(sound type: Sound) {
+        
+        guard let soundPath = Bundle.main.path(forResource: type.rawValue, ofType: "mp3") else {
+            print("No sound file")
+            return
+        }
+        
+        let alertSound = URL(fileURLWithPath: soundPath)
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            try audioPlayer = AVAudioPlayer(contentsOf: alertSound)
+
+        } catch {
+            
+            dump(error)
+        }
+        
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+    
+    @IBAction func playSound() {
+     
+        play(sound: .five)
     }
 
 }
