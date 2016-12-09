2f6b2acfce2d9ccedf1e7693fe291ad6e7c35ad3
Jose Torres <jose.torres@santexgroup.com>
2016-12-09 10:51:06 -0500

    Added RateViewContorller

---
+++ nosedive/RateViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RateViewController.swift
+//  nosedive
+//
+//  Created by Santex on 12/9/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
